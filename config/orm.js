caaa762ec4f58374cff8a93deb0865c1754536d9
Albert Factora <albert.factora@gmail.com>
2017-09-26 10:01:43 -0700

    More path fix

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,5 @@
+var path = require("path");
+
 //var connection = require("./connection");
 var connection = require(path.join(__dirname,"..", "config", "connection.js"));
 function printQuestionMarks(num) {
