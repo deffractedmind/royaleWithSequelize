00ab91d1a5b64ed12a4ef671434456298b64835b
Albert Factora <albert.factora@gmail.com>
2017-09-26 15:18:48 -0700

    Added Sequelize package

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,15 @@
-//var orm = require("../config/orm");
-var path = require("path");
-var orm = require(path.join(__dirname,"..", "config", "orm.js"));
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(name, cb) {
-    orm.create("burgers", ["burger_name", "devoured"], [name, false], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
-  }
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: DataTypes.STRING,
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+      }
+    }
+  });
+  return Burger;
 };
-module.exports = burger;
