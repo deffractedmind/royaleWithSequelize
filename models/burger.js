14e040682581c276d6b13633604588e22690819d
Albert Factora <albert.factora@gmail.com>
2017-09-26 15:39:29 -0700

    With sequelize

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,8 @@
+// Burger models
+
+// The burger has a burger_name attribute of type DataTypes.String
+// and a devoured attribute that is false by default
+
 module.exports = function(sequelize, DataTypes) {
   var Burger = sequelize.define("Burger", {
     burger_name: DataTypes.STRING,
@@ -8,6 +13,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
+        // associations can be defined here
       }
     }
   });
--- server.js
+++ server.js
@@ -1,30 +1,59 @@
+// Solution 1: Sequelize the burger assignment
+// ===========================================
+
+// This solutions adds sequelize functionality to Burger
+// This is the minimum required work for students
+
+// Step 1: Deleted the models, db, and config folder
+
+// Step 2: Ran sequelize init:config init:models in the command line to initalize the project
+
+// Step 2: Edited the new config.json file to accomodate our database connection
+
+// Step 3: Made a burger model with a burger_name attribute of type DataTypes.String, and a devoured attribute of type
+// DataTypes.Boolean. Set devoured to have a defaultValue of false
+
+// Step 5: Removed any reference to the old ORM in burgers_controller
+
+// Step 6: Utilized Sequelize ORM methods in place of the deleted ORM functions
+//         in burgers_controller.js
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var path = require("path");
 
-var port = process.env.PORT || 3000;
+// bring in the models
+var db = require("./models");
 
 var app = express();
-
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + "./public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+// override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
-
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require(path.join(__dirname,"controllers","burgers_controller.js"));
+var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
 
-app.listen(port);
-console.log("Listening on port: " + port);
+
+// listen on port 3000
+var port = process.env.PORT || 3000;
+db.sequelize.sync().then(function() {
+  app.listen(port);
+});
+
+
+console.log(module.exports);
